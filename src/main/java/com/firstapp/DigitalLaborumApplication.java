6646fe5f5f67c3f0af916c470ce759f769443f7a
Valentina Barrera Lagos <valentina.barrera1601@alumnos.ubiobio.cl>
2021-09-02 20:24:47 +0200

    reupload project to github

---
+++ src/main/java/com/firstapp/DigitalLaborumApplication.java
@@ -0,0 +1,13 @@
+package com.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLaborumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLaborumApplication.class, args);
+	}
+
+}
